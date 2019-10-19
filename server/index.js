af35ff0912af6c2d6ae14fdd21e011a956a75d77
Troy <troyjmcl85@gmail.com>
2019-10-19 12:01:28 -0600

    All 4 services rendering info for same product

--- server/index.js
+++ server/index.js
@@ -5,26 +5,24 @@ const proxy = require('express-http-proxy');
 const app = express();
 const port = 3000
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'public'))); 
 
 app.use('/description', proxy(`http://localhost:3002`,{
   proxyReqPathResolver: function (req) {
     var prod = req.url.split('?');
     var pram = prod[1]
-    console.log('inside desc', pram)
     return `/description?${pram}`
   }
-}))
+}));
 
 app.use('/reviews', proxy('http://localhost:3004', {
   proxyReqPathResolver: function (req) {
     var prod = req.url.split('?');
     var pram = prod[1]
-    console.log('inside reviews', pram)
     return `/reviews?${pram}`
   }
-}))
+}));
 
 app.use('/images', proxy(`http://localhost:3003`, {
   proxyReqPathResolver: function (req) {
@@ -32,14 +30,14 @@ app.use('/images', proxy(`http://localhost:3003`, {
     var pram = prod[1]
     return `/images?${pram}`
   }
-}))
+}));
 
 app.use('/suggested', proxy('http://localhost:3001', {
   proxyReqPathResolver: function (req) {
     var prod = req.url.split('?');
-    console.log('inside sugg', prod)
-    return `/suggested?${prod}`
+    var pram = prod[1]
+    return `/suggested?${pram}`
   }
-}))
+}));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
